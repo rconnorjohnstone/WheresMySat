84398113ddf8ec6078f776a917a6ac9618283fc6
Connor Johnstone <connor@richardconnorjohnstone.com>
2019-02-23 21:20:43 -0700

    Finished making comments from before, added ad and sigma values

--- measurements.py
+++ measurements.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 ''' Definition of the measurement class for the CU Hackathon Project: WheresMySat
 
 Parent class meas records a measurement to be fed into the Kalman Filter
@@ -8,26 +9,43 @@ Author:Connor Johnstone
 '''
 
 #Import Libraries
-import numpy as np
-from numpy import linalg as LA
+from abc import ABC, abstractmethod
+import ad
 
-class Meas():
+class Meas(ABC):
     ''' Measurement Class
     
     Records all data common to all measurements our Kalman Filter should expect to see
     
     Arguments:
-         - value: (list of np.ndarray) of measurement values
+         - value: (float64) Measurement value
+         - sigma: (float64) Measurement variance
          - time: (float64) timestamp of measurements in seconds from reference epoch
          - mission_id: (string) name of mission to match records to
          
     Measurement class is generally considered incomplete without an additional child class for
     the measurement type
     '''
-    def __init__(self,value,time,mission_id):
-        self.val = value
+    def __init__(self,value,time,mission_id,sigma):
+        self.value = value
+        self.sigma = sigma
         self.t = time
         self.mis_id = mission_id
+        
+    @abstractmethod
+    def gen_meas(self):
+        ''' The method used to generate a measurement from expected state data
+        
+        This method must be overwritten by the child class
+        '''
+        pass
+    
+    def residual(self,state):
+        ad_state = ad.adnumber(state)
+        est_msr = self.gen_meas(ad_state)
+        resid = self.value - est_msr.real
+        H_tilde = ad.jacobian(est_msr, ad_state)
+        return resid,H_tilde
     
         
 
--- sample.py
+++ sample.py
@@ -1,13 +1,61 @@
 import RRE_measurements as RRE
+import numpy as np
 
-range, range_rate, elev = 300, 1.2, 40
-latitude, longitude, height, time = 20,30,58537,7000
-station1 = RRE.station(latitude, longitude, height)
-range1 = RRE.RangeMeas(range, time, 'New Horizons', station1)
-range_rate1 = RRE.RangeRateMeas(range_rate, time, 'New Horizons', station1)
-elev1 = RRE.ElevMeas(elev, time, 'New Horizons', station1)
+mission1, range1, range_rate1, elev1, sigma1 = "ISS",150, 1.2, 40, 0.01
+mission2, range2, range_rate2, elev2, sigma2 = "Soviet Spy Satellite",200, 1.5, -20, 0.04
+
+station1 = RRE.station("Boulder", 40.0, -105.2, 5400)
+station2 = RRE.station("Louisville", 38.25, -85.75, 460)
+
+t1, t2 = 58537.863889, 41988.1875
+
+range_meas1 = RRE.RangeMeas(range1, t1, mission1, station1)
+range_rate_meas1 = RRE.RangeRateMeas(range_rate1, t1, mission1, station1)
+elev_meas1 = RRE.ElevMeas(elev1, t1, mission1, station1)
+
+range_meas2 = RRE.RangeMeas(range2, t1, mission2, station1)
+range_rate_meas2 = RRE.RangeRateMeas(range_rate2, t1, mission2, station1)
+elev_meas2 = RRE.ElevMeas(elev2, t1, mission2, station1)
+
+range_meas3 = RRE.RangeMeas(range1, t2, mission1, station2)
+range_rate_meas3 = RRE.RangeRateMeas(range_rate1, t2, mission1, station2)
+elev_meas3 = RRE.ElevMeas(elev1, t2, mission1, station2)
+
+state1 = np.array([8000,1000,2000,1,6.5,-1])
+state2 = np.array([6000,3000,-2000,1.9,6.5,-1.2])
+
+range_resid1 = range_meas1.residual(state1)
+range_rate_resid1 = range_rate_meas1.residual(state1)
+elev_resid1 = elev_meas1.residual(state1)
+
+range_resid2 = range_meas3.residual(state2)
+range_rate_resid2 = range_rate_meas3.residual(state2)
+elev_resid2 = elev_meas3.residual(state2)
 
 print(station1)
-print(range1)
-print(range_rate1)
-print(elev1)
+print(station1.get_ECI(t1))
+print('')
+print(station2)
+print(station2.get_ECI(t2))
+print('')
+
+print(range_meas1)
+print(range_rate_meas1)
+print(elev_meas1)
+print('')
+print(range_meas2)
+print(range_rate_meas2)
+print(elev_meas2)
+print('')
+print(range_meas3)
+print(range_rate_meas3)
+print(elev_meas3)
+print('')
+
+print(range_resid1)
+print(range_rate_resid1)
+print(elev_resid1)
+print('')
+print(range_resid2)
+print(range_rate_resid2)
+print(elev_resid2)
