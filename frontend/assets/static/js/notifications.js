345e6a650c9e0e29bffb7e2c3579e4eb0a701dc4
Hunter Mellema <mellemahp@gmail.com>
2019-05-11 21:44:37 -0600

    Finalized messages being converted to semantic ui

--- frontend/assets/static/js/notifications.js
+++ frontend/assets/static/js/notifications.js
@@ -1,13 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-	(document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
-		$notification = $delete.parentNode;
-		$delete.addEventListener('click', () => {
-		$notification.parentNode.removeChild($notification);
-		});
-	});
-});
-
-document.addEventListener('DOMContentLoaded', () => {
 	(document.querySelectorAll('.message .close') || []).forEach(($delete) => {
 		$notification = $delete.parentNode;
 		$delete.addEventListener('click', () => {
